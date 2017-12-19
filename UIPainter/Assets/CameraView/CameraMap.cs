23feb76009f73cb20f1a0532cd5b2a9c78853af3
yujie <905904137@qq.com>
2017-12-19 18:49:11 +0800

    first commit

---
+++ UIPainter/Assets/CameraView/CameraMap.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMap : MonoBehaviour {
+
+    Map tmpMap;
+
+   public  Terrain terrainData;
+
+
+    RectTransform selfTrans;
+    void Start()
+    {
+
+        tmpMap = Camera.main.GetComponent<Map>();
+
+ 
+          selfTrans =  transform.GetComponent<RectTransform>();
+    }
+    public int lineCount = 100;
+    public float radius = 3.0f;
+
+    static Material lineMaterial;
+    static void CreateLineMaterial()
+    {
+        if (!lineMaterial)
+        {
+            // Unity has a built-in shader that is useful for drawing
+            // simple colored things.
+            Shader shader = Shader.Find("Hidden/Internal-Colored");
+            lineMaterial = new Material(shader);
+            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+            // Turn on alpha blending
+            lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            // Turn backface culling off
+            lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+            // Turn off depth writes
+            lineMaterial.SetInt("_ZWrite", 1);
+        }
+    }
+
+
+    public Vector2 ConvertPoints(int index)
+    {
+
+        index = index % tmpMap.rayPoints.Length;
+
+        Vector3 tmpPos = tmpMap.rayPoints[index];
+
+
+
+        float rateXX = tmpPos.x / terrainData.terrainData.size.x;
+
+        float rateYY = tmpPos.z / terrainData.terrainData.size.z;
+
+
+        rateXX -= 0.5f;
+
+        rateYY -= 0.5f;
+
+        Vector2 result = Vector2.zero;
+       result.x=  selfTrans.sizeDelta.x* rateXX;
+
+       result.y = selfTrans.sizeDelta.y * rateYY;
+
+      // Debug.Log("index=="+index +"==reuslt=="+result);
+
+
+
+       return result;
+
+    }
+    // Will be called after all regular rendering is done
+    public void OnRenderObject()
+    {
+        CreateLineMaterial();
+        // Apply the line material
+        lineMaterial.SetPass(0);
+
+      
+        GL.PushMatrix();
+        // Set transformation matrix for drawing to
+        // match our transform
+        GL.MultMatrix(transform.localToWorldMatrix);
+
+
+        //GL.LoadOrtho();
+        // Draw lines
+        GL.Begin(GL.LINES);
+
+
+        GL.Color(Color.red);
+
+        for (int i = 1; i < 5; i++)
+        {
+   
+
+
+            Vector2 front = ConvertPoints(i - 1);
+
+
+
+            GL.Vertex3(front.x, front.y, 0);
+            Vector2 back = ConvertPoints(i);
+
+            GL.Vertex3(back.x, back.y, 0);
+            
+        }
+ 
+  
+        GL.End();
+        GL.PopMatrix();
+    }
+}
---
+++ UIPainter/Assets/CameraView/Map.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        rayPoints = new Vector3[4] ;
+
+        for (int i = 0; i < 4; i++)
+        {
+            rayPoints[i] = Vector3.zero;
+        }
+		
+	}
+
+
+    public Vector3[] rayPoints;
+	// Update is called once per frame
+	void Update () {
+
+
+      Ray   tmpRay=    Camera.main.ViewportPointToRay(Vector3.zero);
+
+
+        RaycastHit  hit  ;
+
+        if (Physics.Raycast(tmpRay, out hit ,1000))
+        {
+
+
+            rayPoints[0] = hit.point;
+
+
+         //   Debug.Log("hit.point 11==" + hit.point);
+             
+ 
+        }
+
+   
+
+      Debug.DrawRay(tmpRay.origin,tmpRay.direction,Color.red);
+
+
+
+      Ray tmpRay2 = Camera.main.ViewportPointToRay(Vector3.right);
+
+      if (Physics.Raycast(tmpRay2, out hit, 1000))
+      {
+
+
+          rayPoints[1] = hit.point;
+
+        //  Debug.Log("hit.point 222==" + hit.point);
+
+
+      }
+
+      Debug.DrawRay(tmpRay2.origin, tmpRay2.direction, Color.red);
+
+
+      Ray tmpRay3 = Camera.main.ViewportPointToRay(Vector3.one);
+
+
+      if (Physics.Raycast(tmpRay3, out hit, 1000))
+      {
+
+
+          rayPoints[2] = hit.point;
+
+          //Debug.Log("hit.point 333==" + hit.point);
+
+
+      }
+
+
+
+      Debug.DrawRay(tmpRay3.origin, tmpRay3.direction, Color.red);
+
+
+      Ray tmpRay4 = Camera.main.ViewportPointToRay(Vector3.up);
+
+
+      if (Physics.Raycast(tmpRay4, out hit, 1000))
+      {
+
+        
+          rayPoints[3] = hit.point;
+
+       //   Debug.Log("hit.point 444==" + hit.point);
+
+
+      }
+
+
+
+      Debug.DrawRay(tmpRay4.origin, tmpRay4.direction, Color.red);
+		
+	}
+}
